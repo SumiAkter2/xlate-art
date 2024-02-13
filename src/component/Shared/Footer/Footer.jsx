f327f11448e42639a69367b0879c180049f6bccf
SumiAkter2 <akteralisha36@gmail.com>
2024-02-13 11:36:54 +0600

    footer done

--- src/component/Shared/Footer/Footer.jsx
+++ src/component/Shared/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { FaXTwitter } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div>
-      <footer className="footer p-10 bg-gray-50 text-secondary">
+      <footer className="footer p-10 bg-gray-50 text-secondary font-semibold font-sans">
         <aside>
           <h1
             className="flex-1  text-3xl font-bold"
@@ -12,12 +12,26 @@ const Footer = () => {
           >
             Exhibit!
           </h1>
-          <p className="text-xl">Helping your museum talk to the world!</p>
+          <p className="text-sm">Helping your museum talk to the world!</p>
           <p className="text-2xl">Questions?</p>
-          <p className="font-bold">
+          <p className="">
             <a href="#">Support@Exhibit.Technology</a>
           </p>
         </aside>
+        <div>
+          <h1 className="footer-title">SUBSCRIBE</h1>
+          <p className="text-sm">
+            Subscribe to our newsletter and get exclusive updates directly in
+            your inbox.
+          </p>
+          <div className="join join-vertical lg:join-horizontal mt-6">
+            <input
+              className="input input-secondary focus:outline-0  join-item"
+              placeholder="Text here"
+            />
+            <button className="btn btn-secondary join-item">Submit</button>
+          </div>
+        </div>
         <nav>
           <h6 className="footer-title">Social</h6>
           <div className="grid grid-flow-col gap-4">
